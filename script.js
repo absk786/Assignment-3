8e02b847b2d12e2fdb6ac8eec4d655c2666a8d20
Abdul Rehman Khalil <abdulrehmankhalil@Abduls-MacBook-Air.local>
2021-05-07 05:36:11 -0400

    copying stuff from old repo to new repo

---
+++ script.js
@@ -0,0 +1,30 @@
+// generate password 
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// THEN I select which criteria to include in the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// THEN my input should be validated and at least one character type should be selected
+// THEN a password is generated that matches the selected criteria
+// THEN the password is either displayed in an alert or written to the page
+
+// list of promts
+// Click here to generate password
+function generatepassword() {
+    alert('This popup shows that the generate password function is working');
+}
+// what is the length of the password
+// i need a function that says choose the number of characters between 8-128. 
+// If the number of characters are between 8-128, this field is valid. 
+// Else promt user to pick valid number of characters
+// Do you want any uppercase letters
+
+
+
+// if user selects yes, add an uppercase letter to the password, else do not
+// Do you want any numbres
+// if user selects yes, add a number to the password, else, do not
+// do you want any special characters
+// if user seelcts yes, add special characters to the password, else do not
+
+
