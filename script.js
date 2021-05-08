298e8ef72c82b652e44bbfe350a9554ae37f2116
Abdul Rehman Khalil <abdulrehmankhalil@Abduls-MacBook-Air.local>
2021-05-07 20:05:14 -0400

    latest

--- script.js
+++ script.js
@@ -1,30 +1,99 @@
-// generate password 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// THEN I select which criteria to include in the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// THEN my input should be validated and at least one character type should be selected
-// THEN a password is generated that matches the selected criteria
-// THEN the password is either displayed in an alert or written to the page
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", charLength);
 
-// list of promts
-// Click here to generate password
-function generatepassword() {
-    alert('This popup shows that the generate password function is working');
+var whatoDisplay = '';
+var charLengthInput;
+var output= '';
+// This function will get the length for the password
+var charLength = function()     {
+// prompt to find out length of password
+charLengthInput = window.prompt( "How long would you like your password to be? Choose a number between 8 and 128 characters.");
+//if valid number the functions stores the value, otherwise it returns the prompt again
+if (charLengthInput >=8 && charLengthInput <=128) {
+    return charLengthInput;
+    }
+else    {
+    window.alert("You need to choose a number greater than 8 and less than 128.");
+    charLength();
+    }
+
+        whatoDisplay = undefined;
 }
-// what is the length of the password
-// i need a function that says choose the number of characters between 8-128. 
-// If the number of characters are between 8-128, this field is valid. 
-// Else promt user to pick valid number of characters
-// Do you want any uppercase letters
+charLength();
+
+// this statement will select which characters they want
+var selectUppercase = window.confirm('Do you want any UPPER case letters in your password?');
+var selectLowercase = window.confirm('Do you want any Lower cases letters in your password?');
+var selectSpecial = window.confirm('Do you want any special letters in your password?');
+var selectNumber = window.confirm('Do you want any numbers in your password?')
+ 
+// this loop runs the generateChars function and is set to run the length of the password
+ for (let i=0; i < charLengthInput; i++) {
+    generateChars(i)
+    }
+// this function checks if the user wants that particular character and then appends it to the output var
+function generateChars () {
+
+        if (selectUppercase===true) { 
+       output +=getrandomUppercase();
+        }
+        if(selectLowercase===true) {
+                output +=getrandomLowercase();
+
+        }
+        if(selectSpecial===true) {
+                output +=getrandomSpecial();
+
+        }
+        if(selectNumber===true) {
+                output +=getrandomNumber();
+        }
+
+ }
+
+// this splits the output into an array of individual characters
+let array = output.split('');
+
+// this loop generates random characters from the arry to store as a final password
+
+for (var a =0; a < charLengthInput; a++) {
+  getarraychar(a);
+}
+function getarraychar () {
+    whatoDisplay += array[randomarraynumber()];
+}
+console.log(whatoDisplay);
+
+// this displays my password
+document.getElementById('finalDisplay').innerHTML = whatoDisplay;
 
 
 
-// if user selects yes, add an uppercase letter to the password, else do not
-// Do you want any numbres
-// if user selects yes, add a number to the password, else, do not
-// do you want any special characters
-// if user seelcts yes, add special characters to the password, else do not
+
+
+
+                // Below are the functions that generate my random numbers//
+// this generates a random number within the array index
+function randomarraynumber () {
+ return Math.floor(Math.random()* output.length);
+}
+// this generates random uppercase letters
+function getrandomUppercase() {
+    return (String.fromCharCode(Math.floor((Math.random()*26) +65)));
+}
+// this generates random lowercase numbers
+function getrandomLowercase() {
+   return String.fromCharCode((Math.floor(Math.random()*26) + 97));
+}
+// this generates random special characters
+function getrandomSpecial() {
+
+return String.fromCharCode((Math.floor(Math.random()*15)+32));
+}
+// this generates random numbers between 0-9
+function getrandomNumber() {
+return String.fromCharCode(Math.floor((Math.random()*10)+48));
+}
+
 
 
